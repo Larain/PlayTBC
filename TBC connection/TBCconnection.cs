39182909271cd6061b3b10954c31f7ca2aa93f2f
Yura Oleynik <Gostwel@gmail.com>
2016-04-19 20:04:54 +0300

    Sleep time increased

--- TBC connection/TBCconnection.cs
+++ TBC connection/TBCconnection.cs
@@ -66,7 +66,7 @@ namespace TBC_connection
                     UpdateNile(Nile);
                     UpdateYangtze(Yangtze);
                     counter++;
-                    Thread.Sleep(15000);
+                    Thread.Sleep(30000);
                 }
                 UpdateProgressBar(repeat);
             }));
